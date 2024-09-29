a9af68c69dc0c974a020c0d5c7c206281308d10f
Chaitanya Belekar <157277524+Chaitanya6Nli@users.noreply.github.com>
2024-09-29 13:39:14 +0530

    Add files via upload

--- week3/setup.py
+++ week3/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pid_controller = week3_assignment.pid_controller:main'
+            'pid_controller = week3_assignment.pid_controller:main', 'week3_assignment.path_planner',
         ],
     },
 )
